8e95d1d284f5928089e9456493f538eac0121136
aryan02420 <aryan02420@gmail.com>
2022-02-09 20:38:31 +0000

    feat: word list, better types, start enc dec func

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,77 @@
+import { getRandomSolution, isValidWord } from './words.js'
+
 namespace Wordle {
+
+  type alphabet = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' |
+                  'g' | 'h' | 'i' | 'j' | 'k' | 'l' |
+                  'm' | 'n' | 'o' | 'p' | 'q' | 'r' |
+                  's' | 't' | 'u' | 'v' | 'w' | 'x' |
+                  'y' | 'z'
   type Tuple5<T> = [T, T, T, T, T]
   type Tuple6<T> = [T, T, T, T, T, T]
-  type TWord = Tuple5<string>
+  type TWord = Tuple5<alphabet | null>
   type TGuesses = Tuple6<TWord>
   enum EFeedback {
     empty = 0,
-    incorrect,
-    position,
+    absent,
+    present,
     correct
   }
   type TFeedback = Tuple5<EFeedback>
-  type TFeedbacks= Tuple6<TFeedback>
+  type TFeedbacks = Tuple6<TFeedback>
   interface IKeyboard {
-    empty: string[]
-    incorrect: string []
-    position: string []
-    correct: string []
+    absent: alphabet[]
+    present: alphabet[]
+    correct: alphabet[]
   }
   interface IState {
     guess: TGuesses
-    feedback :TFeedbacks
+    feedback: TFeedbacks
     keyboard: IKeyboard
     rowIndex: number
     colIndex: number
-    solution: TWord
+    solution: Tuple5<alphabet>
   }
+
   function start(seed: string): IState {
-    return {} as IState
+    return {
+      guess: [
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+      ],
+      feedback: [
+        [EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty],
+        [EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty],
+        [EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty],
+        [EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty],
+        [EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty],
+        [EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty, EFeedback.empty],
+      ],
+      keyboard: {
+        absent: [],
+        present: [],
+        correct: [],
+      },
+      rowIndex: 0,
+      colIndex: 0,
+      solution: getRandomSolution().split('') as Tuple5<alphabet>
+    }
   }
+
   function encode(state: IState): string {
-    return ''
+    return Buffer.from(JSON.stringify(state), 'binary').toString('base64')
   }
+
   function decode(str: string): IState {
-    return {} as IState
+    return JSON.parse(Buffer.from(str, 'base64').toString('binary')) as IState
   }
+
   function action(state: IState, move: string): IState {
+
     return state
   }
 }
