23607ab8f1686c52d21de0ab7115baba2e186187
aryan02420 <aryan02420@gmail.com>
2022-02-11 07:13:48 +0000

    test: add tests for words.ts

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+const config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+}
+
+module.exports = config
---
+++ test/words.test.ts
@@ -0,0 +1,17 @@
+import { SolutionWords, ValidWords, getRandomSolution, isValidWord } from '../src/words'
+
+test('return random word', () => {
+    expect(getRandomSolution(undefined)).not.toBe(getRandomSolution(undefined))
+})
+
+test('solution word is valid word', () => {
+    expect(isValidWord(SolutionWords[0])).toBe(true)
+})
+
+test('valid word is valid word', () => {
+    expect(isValidWord(ValidWords[0])).toBe(true)
+})
+
+test('non word is invalid', () => {
+    expect(isValidWord('aaaaa')).toBe(false)
+})
