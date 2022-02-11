6a1a4888d3d226de8c751b64333da7fa0b5b0217
aryan02420 <aryan02420@gmail.com>
2022-02-11 05:12:51 +0000

    feat: complete checkAndUpdateState

--- src/utils.ts
+++ src/utils.ts
@@ -55,5 +55,41 @@ export function getNewState(seed: any): IState {
 }
 
 export function checkAndUpdateState(state: IState): IState {
-    return {} as IState
+
+    const input_word = state.wrd[state.row].join('')
+
+    if (input_word.length !== 5) {
+        state.msg = 'Not enough letters'
+        return state
+    }
+    
+    if (isValidWord(input_word)) {
+        state.msg = 'Not a word'
+        return state
+    }
+
+    state.wrd[state.row].forEach((alpha, i) => {
+
+        if (state.sol.findIndex(val => val === alpha) === -1) {
+            state.fbk[state.row][i] = EFeedback.absent
+            state.kbd[alpha!] = Math.max(EFeedback.absent, state.kbd[alpha!])
+        } else {
+            state.fbk[state.row][i] = EFeedback.present
+            state.kbd[alpha!] = Math.max(EFeedback.present, state.kbd[alpha!])
+        }
+
+        if (alpha === state.sol[i]) {
+            state.fbk[state.row][i] = EFeedback.correct
+            state.kbd[alpha!] = Math.max(EFeedback.correct, state.kbd[alpha!])
+        }
+        
+    })
+
+    if (input_word === state.sol.join('')) {
+        state.msg = 'Great!!!'
+        state.fin = true
+        return state
+    }
+
+    return state
 }
