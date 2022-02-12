01024418453ac78bb43d82e26e7c39eff59c20e5
aryan02420 <aryan02420@gmail.com>
2022-02-12 07:02:05 +0000

    test: add tests for utils.ts

---
+++ test/utils.test.ts
@@ -0,0 +1,92 @@
+import { removeAlphabet, addAplhabet } from '../src/utils'
+
+test('backspace deletes last character', () => {
+    const state = {
+        row: 0,
+        col: 3,
+        wrd: [['a', 'b', 'c', null, null]],
+        msg: '',
+    }
+    const stateNew = {
+        row: 0,
+        col: 2,
+        wrd: [['a', 'b', null, null, null]],
+        msg: '',
+    }
+    // @ts-ignore
+    expect(removeAlphabet(state)).toEqual(stateNew)
+})
+
+test('backspace does nothing when no alphabet in current guess', () => {
+    const state = {
+        row: 1,
+        col: 0,
+        wrd: [['a', 'b', 'c', 'd', 'e'], [null, null, null, null, null]],
+        msg: '',
+    }
+    const stateNew = {
+        row: 1,
+        col: 0,
+        wrd: [['a', 'b', 'c', 'd', 'e'], [null, null, null, null, null]],
+        msg: '',
+    }
+    // @ts-ignore
+    expect(removeAlphabet(state)).toEqual(stateNew)
+})
+
+test('backspace clears message', () => {
+    const state = {
+        row: 0,
+        col: 5,
+        wrd: [['a', 'b', 'c', 'd', 'e']],
+        msg: 'some error msg',
+    }
+    // @ts-ignore
+    expect(removeAlphabet(state).msg).toBe('')
+})
+
+test('alphabet adds a new character', () => {
+    const state = {
+        row: 0,
+        col: 3,
+        wrd: [['a', 'b', 'c', null, null]],
+        msg: '',
+    }
+    const stateNew = {
+        row: 0,
+        col: 4,
+        wrd: [['a', 'b', 'c', 'd', null]],
+        msg: '',
+    }
+    // @ts-ignore
+    expect(addAplhabet(state, 'd')).toEqual(stateNew)
+})
+
+test('alphabet does nothing when current guess is full', () => {
+    const state = {
+        row: 0,
+        col: 5,
+        wrd: [['a', 'b', 'c', 'd', 'e']],
+        msg: '',
+    }
+    const stateNew = {
+        row: 0,
+        col: 5,
+        wrd: [['a', 'b', 'c', 'd', 'e']],
+        msg: '',
+    }
+    // @ts-ignore
+    expect(addAplhabet(state, 'd')).toEqual(stateNew)
+})
+
+test('alphabet clears message', () => {
+    const state = {
+        row: 0,
+        col: 3,
+        wrd: [['a', 'b', 'c', null, null]],
+        msg: 'some error message',
+    }
+    // @ts-ignore
+    expect(addAplhabet(state, 'd').msg).toBe('')
+})
+
