f3685a3a81286bf1ce42a5b1b14c08242900ee60
aryan02420 <aryan02420@gmail.com>
2022-02-17 19:59:20 +0000

    feat: inbuilt message strings

---
+++ src/message.ts
@@ -0,0 +1,13 @@
+export const messages: Record<EMessages, string[]> = {
+  [EMessages.none]: [''],
+  [EMessages.less]: ['not enough characters', 'guess must be a five letter word'],
+  [EMessages.invalid]: ['not a valid word', 'word not valid'],
+  [EMessages.win]: ['Great', 'Awesome', 'You Win'],
+  [EMessages.lose]: [''],
+}
+
+export function getMessageString(type: EMessages): string {
+  const msgs = messages[type]
+  const index =  Math.floor(Math.random()*msgs.length)
+  return msgs[index]
+}
