5fec3acf741ac7e6e2993c362c3f9ed69c0e9cec
aryan02420 <aryan02420@gmail.com>
2022-07-31 17:35:13 +0000

    feat!: better encoding

--- src/core/encoder.ts
+++ src/core/encoder.ts
@@ -1,163 +1,153 @@
+// format
+
+// row col msg fin sol kbd wrd fbk
+
+//    bits    start   end   type    value   data  
+//    3       0       3     row     001     1
+//    3       3       6     col     001     1
+//    3       6       9     msg     000     none
+//    1       9       10    fin     0       false
+//    5       10      15    sol[0]  00010   't'
+//    5       15      20    sol[1]  00000   'u'
+//    5       20      25    sol[2]  00000   'l'
+//    5       25      30    sol[3]  00000   'i'
+//    5       30      35    sol[4]  00000   'p'
+//    2       35      37    kbd.a   00      empty
+//    2       37      39    kbd.b   00      empty
+//    2       39      41    kbd.c   00      empty
+//    2       41      43    kbd.d   00      empty
+//    2       43      45    kbd.e   00      empty
+//    2       45      47    kbd.f   00      empty
+//    2       47      49    kbd.g   00      empty
+//    2       49      51    kbd.h   00      empty
+//    2       51      53    kbd.i   00      empty
+//    2       53      55    kbd.j   00      empty
+//    2       55      57    kbd.k   00      empty
+//    2       57      59    kbd.l   00      empty
+//    2       59      61    kbd.m   00      empty
+//    2       61      63    kbd.n   00      empty
+//    2       63      65    kbd.o   00      empty
+//    2       65      67    kbd.p   00      empty
+//    2       67      69    kbd.q   00      empty
+//    2       69      71    kbd.r   00      empty
+//    2       71      73    kbd.s   00      empty
+//    2       73      75    kbd.t   00      empty
+//    2       75      77    kbd.u   00      empty
+//    2       77      79    kbd.v   00      empty
+//    2       79      81    kbd.w   00      empty
+//    2       81      83    kbd.x   00      empty
+//    2       83      85    kbd.y   00      empty
+//    2       85      87    kbd.z   00      empty
+//    5       87      92    wrd0:0  00000   'c'
+//    5       92      97    wrd0:1  00000   'r'
+//    5       97      102   wrd0:2  00000   'a'
+//    5       102     107   wrd0:3  00000   'n'
+//    5       107     112   wrd0:4  00000   'e'
+//    5       112     117   wrd1:1  00000   'g'
+//    2       117     119   fbk0:0  00      absent
+//    2       119     121   fbk0:1  00      absent
+//    2       121     123   fbk0:2  00      absent
+//    2       123     125   fbk0:3  00      absent
+//    2       125     127   fbk0:4  00      absent  
+
+// 000 000 000 0 00000 01000 10010 01011 00100 00000000000000000000000000 00000000000000000000000000 000
+
 export function serialize(state: IState): string {
   let str = ''
 
-  str += state.row
-  str += state.col
-  str += Number(state.fin)
-  str += state.msg
-  str += state.sol.join('')
+  // row col msg fin
+  str += state.row.toString(2).padStart(3, '0')
+  str += state.col.toString(2).padStart(3, '0')
+  str += state.msg.toString(2).padStart(3, '0')
+  str += Number(state.fin).toString(2)
 
+  // sol
+  str += asciiToBin(state.sol.join(''))
+
+  // kbd
   str += Object.entries(state.kbd)
     .sort((a, b) => a[0].localeCompare(b[0]))
-    .map((x) => x[1])
+    .map(x => x[1])
+    .map(x => x.toString(2).padStart(2, '0'))
     .join('')
 
-  str += state.wrd.map((chars) => chars.join('')).join('')
+  // wrd
+  str += asciiToBin(state.wrd.map((chars) => chars.join('')).join(''))
 
+  // fbk
   str += state.fbk
     .slice(0, state.row)
-    .map((chars) => chars.join(''))
+    .map(row => row.map(x => x.toString(2).padStart(2, '0')).join(''))
     .join('')
 
-  return ascToHex(str)
+  return binToB32(str)
 }
 
 export function deserialize(str: string): IState {
   const state = {} as IState
-  str = hexToAsc(str)
+  str = b32ToBin(str)
+  let cursor = 0
 
-  state.row = +str[0]
-  state.col = +str[1]
-  state.fin = Boolean(+str[2])
-  state.msg = +str[3]
-  state.sol = str.slice(4, 9).split('') as Tuple5<TAlphabet>
+  const consume = (x: number) => {
+    return parseInt(str.slice(cursor, cursor += x).padEnd(x, '0'), 2)
+  }
 
+  // row col msg fin
+  state.row = consume(3)
+  state.col = consume(3)
+  state.msg = consume(3)
+  state.fin = Boolean(consume(1))
+
+  // sol
+  state.sol = String.fromCharCode(...new Array(5).fill(null).map(_ => consume(5) + 97)).split('') as Tuple5<TAlphabet>
+
+  // kbd
   state.kbd = Object.fromEntries(
-    str
-      .slice(9, 35)
-      .split('')
-      .map((x) => +x)
-      .map((x, i) => {
-        return [String.fromCharCode(i + 97), x]
-      })
+    new Array(26).fill(null)
+      .map((_, i) => [String.fromCharCode(i + 97), consume(2)])
   ) as TKeyboard
 
+  // wrd
   const wordsLen = state.row * 5 + state.col
-  const words = [
-    ...str.slice(35, 35 + wordsLen).split(''),
-    ...new Array(30 - wordsLen).fill(null),
-  ]
+  const words = String.fromCharCode(...new Array(wordsLen).fill(null).map(_ => consume(5) + 97)).split('')
+  const nullWords = new Array(30 - wordsLen).fill(null)
+  const allWords = [...words, ...nullWords]
   const wrd: string[][] = []
-  while (words.length) wrd.push(words.splice(0, 5))
+  while (allWords.length) wrd.push(allWords.splice(0, 5))
   state.wrd = wrd as TGuesses
 
+  // fbk
   const feedbacksLen = state.row * 5
-  const feedbacks = [
-    ...str
-      .slice(35 + wordsLen, 35 + wordsLen + feedbacksLen)
-      .split('')
-      .map((x) => +x),
-    ...new Array(30 - feedbacksLen).fill(EFeedback.empty),
-  ]
+  const feedbacks = new Array(feedbacksLen).fill(null).map(_ => consume(2))
+  const emptyFeedbacks = new Array(30 - feedbacksLen).fill(EFeedback.empty)
+  const allFeedbacks = [...feedbacks, ...emptyFeedbacks]
   const fbk: number[][] = []
-  while (feedbacks.length) fbk.push(feedbacks.splice(0, 5))
+  while (allFeedbacks.length) fbk.push(allFeedbacks.splice(0, 5))
   state.fbk = fbk as TFeedbacks
 
   return state
 }
 
-// CONVERSION TABLE
 
-//    char   code       dec     actual
-//    0	     00000	    0       0
-//    1	     00001	    1       1
-//    2	     00010	    2       2
-//    3	     00011	    3       3
-//    4	     00100	    4       4
-//    5	     00101	    5       5
-//    a	     00110	    6       97
-//    b	     00111	    7       98
-//    c	     01000	    8       99
-//    d	     01001	    9       100
-//    e	     01010	    10      101
-//    f	     01011	    11      102
-//    g	     01100	    12      103
-//    h	     01101	    13      104
-//    i	     01110	    14      105
-//    j	     01111	    15      106
-//    k	     10000	    16      107
-//    l	     10001	    17      108
-//    m	     10010	    18      109
-//    n	     10011	    19      110
-//    o	     10100	    20      111
-//    p	     10101	    21      112
-//    q	     10110	    22      113
-//    r	     10111	    23      114
-//    s	     11000	    24      115
-//    t	     11001	    25      116
-//    u	     11010	    26      117
-//    v	     11011	    27      118
-//    w	     11100	    28      119
-//    x	     11101	    29      120
-//    y	     11110	    30      121
-//    z	     11111	    31      122
+export function asciiToBin(char: string): string {
+  if (char.length === 0) return ''
+  if (char.length === 1) {
+    if (char < 'a' || char > 'z') return ''
+    return (char.charCodeAt(0) - 97).toString(2).padStart(5, '0')
+  }
+  return char.split('').map(c => asciiToBin(c)).join('')
+}
 
-export function ascToBin5(str: string): string {
-  return str
+export function binToB32(binStr: string): string {
+  return binStr
+    .match(/[01]{1,5}/g)
+    ?.map(num => parseInt(num.padEnd(5, '0'), 2).toString(32))
+    .join('') ?? ''
+}
+
+export function b32ToBin(b32Str: string): string {
+  return b32Str
     .split('')
-    .map((s) => {
-      if (s >= '0' && s <= '5') {
-        return (+s).toString(2).padStart(5, '0')
-      } else if (s >= 'a' && s <= 'z') {
-        return (s.charCodeAt(0) - 91).toString(2).padStart(5, '0')
-      } else {
-        throw Error(`invalid character: ${s}`)
-      }
-    })
+    .map(num => parseInt(num, 32).toString(2).padStart(5, '0'))
     .join('')
-}
-
-export function bin5ToAsc(str: string): string {
-  return (
-    str
-      .match(/[01]{5}/g)
-      ?.map((c) => {
-        let d = parseInt(c, 2)
-        if (d <= 5) {
-          return String(d)
-        } else if (d >= 6) {
-          return String.fromCharCode(d + 91)
-        }
-      })
-      .join('') ?? ''
-  )
-}
-
-export function bin5ToHex(str: string): string {
-  return (
-    str
-      .match(/[01]{1,4}/g)
-      ?.map((b) => {
-        return parseInt(b.padEnd(4, '0'), 2).toString(16)
-      })
-      .join('') ?? ''
-  )
-}
-
-export function hexToBin5(str: string): string {
-  str = str
-    .split('')
-    .map((h) => {
-      return parseInt(h, 16).toString(2).padStart(4, '0')
-    })
-    .join('')
-  return str.slice(0, str.length - (str.length % 5))
-}
-
-export function ascToHex(str: string): string {
-  return bin5ToHex(ascToBin5(str))
-}
-
-export function hexToAsc(str: string): string {
-  return bin5ToAsc(hexToBin5(str))
 }
--- test/core.encoder.test.ts
+++ test/core.encoder.test.ts
@@ -2,32 +2,29 @@ import { getNewState } from '../src/core/actions'
 import {
   serialize,
   deserialize,
-  ascToBin5,
-  bin5ToAsc,
-  bin5ToHex,
-  hexToBin5,
-  ascToHex,
-  hexToAsc,
+  binToB32,
+  b32ToBin,
 } from '../src/core/encoder'
 
-test('ASCII to bin', () => {
-  expect(ascToBin5('05az')).toBe('00000001010011011111')
-})
+// test('bin to base32', () => {
+//   expect(binToB32('01')).toBe('05az')
+// })
 
-test('bin to ASCII', () => {
-  expect(bin5ToAsc('00000001010011011111')).toBe('05az')
-})
-test('bin to hex', () => {
-  expect(bin5ToHex('0011011001')).toBe('364')
-})
+// test('b32 to bin', () => {
+//   expect(binToB32('05az')).toBe('00000001010011011111')
+// })
 
-test('hex to bin', () => {
-  expect(hexToBin5('364')).toBe('0011011001')
-})
+// test('bin to hex', () => {
+//   expect(bin5ToHex('0011011001')).toBe('364')
+// })
 
-test('correctly encode/decode string', () => {
-  expect(hexToAsc(ascToHex('1234lmno'))).toBe('1234lmno')
-})
+// test('hex to bin', () => {
+//   expect(hexToBin5('364')).toBe('0011011001')
+// })
+
+// test('correctly encode/decode string', () => {
+//   expect(hexToAsc(ascToHex('1234lmno'))).toBe('1234lmno')
+// })
 
 test('correctly serialize/deserialize state #1', () => {
   const state = getNewState()
@@ -35,6 +32,13 @@ test('correctly serialize/deserialize state #1', () => {
 })
 
 test('correctly serialize/deserialize state #2', () => {
-  const state = '18023c4752a8401004000002018c201802300c0000408b9a6a7ea55f623a95421084211084118c6318'
-  expect(serialize(deserialize(state))).toEqual(state)
+  const state = getNewState()
+  const s1 = serialize(state)
+  const s2 = serialize(deserialize(s1)) 
+  expect(s1).toEqual(s2)
 })
+
+// test('correctly serialize/deserialize state #2', () => {
+//   const state = '18023c4752a8401004000002018c201802300c0000408b9a6a7ea55f623a95421084211084118c6318'
+//   expect(serialize(deserialize(state))).toEqual(state)
+// })
